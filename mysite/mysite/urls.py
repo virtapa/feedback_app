7ac7b05618059362534e09801b09c65a0142cb57
jesse19111 <jesse.ma.lindell@student.hamk.fi>
2021-02-09 11:04:58 +0200

    jotain muutoksia

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('signup/', account_views.signup, name='signup'),
+    
 ]
